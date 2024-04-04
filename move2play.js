ecf53b3c8f744579ac423b8762057c065d9313d7
tokoroten <shinta.nakayama@gmail.com>
2024-04-04 13:16:07 +0900

    request permission

--- move2play.js
+++ move2play.js
@@ -60,7 +60,20 @@ function addEnery(event) {
 }
 
 window.onload = () => {
-    window.addEventListener("devicemotion", getAccel);
     document.getElementById("addButton").addEventListener("click", addEnery);
     setInterval(countDown, 100);
+
+    // request permission
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    window.addEventListener("devicemotion", getAccel);
+                }
+            })
+            .catch(console.error);
+    } else {
+        // handle regular non iOS 13+ devices
+        window.addEventListener("devicemotion", getAccel);
+    }
 }
